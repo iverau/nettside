7e9bd6943363c5e0e0de3c716fe6a977fb62a89c
iver <iverau@stud.ntnu.no>
2019-08-07 16:00:13 +0200

    Added templates

--- flask_project/flaskblog.py
+++ flask_project/flaskblog.py
@@ -1,7 +1,28 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
+
+posts = [
+    {
+        'author': "Iver",
+        'title': "Post 1",
+        'content': "First post content",
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': "Knut",
+        'title': "Post 2",
+        'content': "Second post content",
+        'date_posted': 'April 20, 2018'
+    }
+    
+]
+
 @app.route("/")
-def hello():
-    return "<h1>Hello World!!!!!!</h1>"
+@app.route("/home")
+def home():
+    return render_template("home.html", posts=posts,title="Test")
+
+@app.route("/about")
+def about():
+    return render_template("about.html", posts=posts,title="Test2")
